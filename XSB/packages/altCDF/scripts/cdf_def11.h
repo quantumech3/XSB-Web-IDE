4fd14887ee216bee6afa42bf640a46c7fbd91082
quantumech3 <quantumech3@gmail.com>
2019-05-27 17:20:04 -0700

    Revert "Added XSB source code"
    This reverts commit 077246566bcbb5aef0dc6eb136e59fc0162c2a50.

--- XSB/packages/altCDF/scripts/cdf_def11.h
+++
@@ -1,3 +0,0 @@
-
-#define TABLEDISA 1
-#define USING_XJ 1
--- XSB/packages/sgml/cc/common.h
+++
@@ -1,7 +0,0 @@
-#include "fetch_file.c"
-#include "parser.c"
-#include "charmap.c" 
-#include "sgmlutil.c"
-#include "xmlns.c"
-#include "model.c"
-#include <assert.h>
