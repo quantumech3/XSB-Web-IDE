5da3b6c2220514168659247e34b2679c2c305456
quantumech3 <quantumech3@gmail.com>
2019-06-08 18:52:08 -0700

    XSB is running through the C interface

--- Analysis/saved.o/main.c
+++ Analysis/saved.o/main.c
@@ -1,4 +1,20 @@
+#include <cinterf.h>
+#include <stdio.h>
+
 int main()
 {
-	
+	if(xsb_init_string("/"))
+	{
+		// xsb_get_init_error_message() returns the last error that occured while XSB initializes the runtime
+        printf("++XSB failed to initialize with error: %s\n", xsb_get_init_error_message());
+        exit(-1);
+	}
+	else // 2) If XSB successfully initialized
+    {
+        // Print 'XSB successfully initialized'
+        printf("XSB successfully initialized!\n");
+ 
+        // Terminate XSB
+        xsb_close();
+    }
 }
