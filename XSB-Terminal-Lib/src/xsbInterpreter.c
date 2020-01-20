395a7301e738b96d9b45d6e35120902b02065c08
quantumech3 <quantumech3@gmail.com>
2020-01-19 22:02:59 -0800

    Fix formatting in xsbInterpreter.c and rebuilt xsbInterpreter.c to WASM

--- XSB-Terminal-Lib/src/xsbInterpreter.c
+++ XSB-Terminal-Lib/src/xsbInterpreter.c
@@ -7,53 +7,74 @@ int i = 0;
 
 void read_async();
 
-void read_success(emscripten_fetch_t *fetch) {
-    char buff[40];
-    int INPUT_LENGTH = (int)fetch->data[0];
+/**
+ * Called when user input is successfully obtained through XHR
+ */ 
+void read_success(emscripten_fetch_t *fetch)
+{
+	// Empty character buffer
+	char buff[256];
 
-    for(int i = 0; i < 40; i++)
-        buff[i] = ' ';
-    
-    for(int i = 0; i < INPUT_LENGTH; i++)
-        buff[i] = fetch->data[i + 1];
+	// Obtain length of user input in characters. The first character of every command passed to C contains the length of the input
+	int INPUT_LENGTH = (int)fetch->data[0];
 
-    buff[INPUT_LENGTH] = '\0';
+	// Set every character of input buffer to ' '
+	for(int i = 0; i < 40; i++)
+		buff[i] = ' ';
+	
+	// Set buff to the value of the user's input
+	for(int i = 0; i < INPUT_LENGTH; i++)
+		buff[i] = fetch->data[i + 1];
 
-    printf("Attempting to execute command of length %i: %s\n", (int)INPUT_LENGTH, buff);
-    if(xsb_command_string(buff))
-    {
-        printf("\nno.\n");
-    }
-    else
-    {
-        printf("\nyes.\n");
-    }
-    
+	// Add null terminator at the end of input buffer so XSB doesnt overrun the users input, read garbage data and crash
+	buff[INPUT_LENGTH] = '\0';
 
-    emscripten_fetch_close(fetch);
-    if (strlen(fetch->data) > 0 || i++ == 5) {
-        read_async();
-    }
+	// Print debug message
+	printf("Attempting to execute command of length %i: %s\n", (int)INPUT_LENGTH, buff);
+
+	// Attempt to execute the user's XSB command. Print 'Yes' if the user's command is true, else print 'no'
+	if(xsb_command_string(buff))
+	{
+		printf("\nno.\n");
+	}
+	else
+	{
+		printf("\nyes.\n");
+	}
+	
+	emscripten_fetch_close(fetch);
+
+	if (strlen(fetch->data) > 0 || i++ == 5) 
+	{
+		read_async();
+	}
 }
 
-void read_fail(emscripten_fetch_t *fetch) {
-    emscripten_fetch_close(fetch);
+void read_fail(emscripten_fetch_t *fetch) 
+{
+	emscripten_fetch_close(fetch);
 }
 
-void read_async() {
-    emscripten_fetch_attr_t attr;
-    emscripten_fetch_attr_init(&attr);
-    strcpy(attr.requestMethod, "GET");
-    attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
-    attr.onsuccess = read_success;
-    attr.onerror = read_fail;
-    emscripten_fetch(&attr, "___terminal::read");
+void read_async() 
+{
+	emscripten_fetch_attr_t attr;
+	emscripten_fetch_attr_init(&attr);
+	strcpy(attr.requestMethod, "GET");
+	attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
+	attr.onsuccess = read_success;
+	attr.onerror = read_fail;
+	emscripten_fetch(&attr, "___terminal::read");
 }
 
-int main() {
-    if(xsb_init_string("/"))
-    {
-        printf("ERROR: XSB failed to initialize");
-    }
-    read_async();
+int main() 
+{
+	// Attempt to initialize XSB
+	if(xsb_init_string("/"))
+	{
+		// Print error message if XSB fails to initialize
+		printf("ERROR: XSB failed to initialize");
+	}
+	
+	// Start user input loop
+	read_async();
 }
