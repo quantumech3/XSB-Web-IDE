b7f92743f92215b41c4fa3b9b058fe4ff0ff4e64
quantumech3 <quantumech3@gmail.com>
2020-10-27 16:58:49 -0400

    Make start script work with XSB Terminal library

--- start_server.py
+++ start_server.py
@@ -5,7 +5,13 @@ server = Flask(__name__)
 # Send a file located in the presentation module when one is requested
 @server.route("/<path:file>")
 def generic_response(file):
-	return send_from_directory("./src/modules/presentation/", file)
+	# The XSB Terminal library currently assumes all library files are at root.
+	if "xsbInterface" in file:	# If a file from the xsbInterface is requested, then send it from ./src/modules/terminal. 
+		if file == "xsbInterface.wasm":
+			return send_from_directory('./src/modules/terminal/', file, mimetype="application/wasm")
+		return send_from_directory('./src/modules/terminal/', file)
+
+	return send_from_directory("./src/", file)
 
 # Send main webpage when "/" url is queried by client
 @server.route("/")
